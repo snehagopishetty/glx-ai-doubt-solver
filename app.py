d379c474d19d5df4d77380eb1dbac9ef7c921b8e
snehagopishetty <g.sneha2@globallogic.com>
2025-05-22 10:49:22 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from bot import get_hint
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -19,4 +20,6 @@ def ask():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    # app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
